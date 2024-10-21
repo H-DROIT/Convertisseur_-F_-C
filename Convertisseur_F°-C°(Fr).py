bc1a6ebaff13c93159698810f50e7ecfc2c9847f
H-DROIT <hectordroit@icloud.com>
2024-10-21 11:02:29 +0200

    Update Convertisseur_F°-C°(Fr).py

--- Convertisseur_F°-C°(Fr).py
+++ Convertisseur_F°-C°(Fr).py
@@ -1,9 +1,9 @@
-#Convertisseur degrés Farenheit en degrés Celsuis
+#Convertisseur degrés Farenheit en degrés Celsius
 
 run = True
 
 while run == True: 
-    temp_F = float(input("Entrez une température en degrés Farenheit (pas besoin d'écrire l'unité)"))
+    temp_F = float(input("Entrez une température en degrés Farenheit (pas besoin d'écrire le symbole)"))
     temp_C = ( ((temp_F) - 32)/1.8 )
-    print(f"La température en Celsuis de {temp_F}°F est de {temp_C}°C.")
+    print(f"La température en Celsius de {temp_F}°F est de {temp_C}°C.")
     print("")
